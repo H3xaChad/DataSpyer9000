6923c49ea9b94717caab4d970ba9c7480149ab80
David Gaudio <ucd3fe@bosch.com>
2024-11-06 11:16:18 +0100

    Added Frontend-Windows

---
+++ FairDataGetter.Client/Window_CustomerProductGroups.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FairDataGetter.Client
+{
+    /// <summary>
+    /// Interaction logic for Window_CustomerProductGroups.xaml
+    /// </summary>
+    public partial class Window_CustomerProductGroups : Window
+    {
+        public Window_CustomerProductGroups()
+        {
+            InitializeComponent();
+        }
+    }
+}
